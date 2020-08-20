b9fc0e5132a08811b6122c4b6f50a8fd2c14e37d
patfm <patfmurray@gmail.com>
2020-08-20 13:14:45 -0400

    add copy, tweak styles based on feedback

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function() {
-  console.log( "ready!" );
-  $("#matrix .element").on('click', function(){
+  $(".matrix .element").on('click', function(){
+    if ($(this).hasClass('disabled')) return;
       $(this).toggleClass('active');
       $("." + $(this).attr('id')).toggleClass('active');;
 
